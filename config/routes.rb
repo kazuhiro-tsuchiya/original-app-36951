8f7471ef0ea7ef741e81aa1483df99df6c6b6448
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-02-16 21:31:26 +0900

    itemsコントローラーの作成、itemsルーティング、一部のviewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "teams#index"
-  resources :teams
+  resources :teams, only: [:index]
   resources :users, only: [:edit, :update]
+  resources :items
 end
