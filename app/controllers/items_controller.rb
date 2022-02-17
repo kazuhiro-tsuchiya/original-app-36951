79f5c6745540ee115d3113173baa7ad8af9bbfb8
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-02-17 21:17:11 +0900

    商品ページのviewとコントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,24 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+  end
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :item_name
+    validates :price
+    validates :quantity_id
+
+    validates :image
+  end
+
+    validates :size_id, numericality: { other_than: 1, message: "can't be blank" } 
+
+  belongs_to :user
 end
