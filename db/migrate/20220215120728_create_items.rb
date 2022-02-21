657b8c23a2b435418e2a65872727f8823c1a1db5
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-02-21 16:21:24 +0900

    商品投稿に不必要なvalidatesの削除とそれにまつわるdbの変更

--- db/migrate/20220215120728_create_items.rb
+++ db/migrate/20220215120728_create_items.rb
@@ -3,9 +3,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :item_name,    null: false
       t.integer :price,       null: false
-      t.integer :quantity_id, null: false
-      t.integer :size_id
-      t.references :user,     null: false, foreign_key: true 
 
       t.timestamps
     end
