14e8c83309de5d73f971c6b92a3d5c6912d4308b
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-02-21 16:21:24 +0900

    商品投稿に不必要なvalidatesの削除とそれにまつわるdbの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :price
-
     validates :image
   end
 
-    validates :quantity_id, numericality: { other_than: 1, message: "can't be blank" } 
+    #validates :quantity_id, numericality: { other_than: 1, message: "can't be blank" } 
 
-  belongs_to :user
-  belongs_to :quantity
-  belongs_to :size
+  #belongs_to :user
   has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,12 +36,8 @@ ActiveRecord::Schema.define(version: 2022_02_21_060900) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.integer "price", null: false
-    t.integer "quantity_id", null: false
-    t.integer "size_id"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -61,5 +57,4 @@ ActiveRecord::Schema.define(version: 2022_02_21_060900) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "items", "users"
 end
