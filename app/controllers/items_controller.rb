de5554685b295e35f51fad91a8271868ccb312ee
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-02-21 21:23:07 +0900

    商品詳細ページのコントローラーのshowとdestroyアクションの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    if current_user.admin?
+      @item = Item.find(params[:id])
+      @item.destroy
+      redirect_to root_path
+    end
+  end
+
+
+
+
   private
 
   def item_params
