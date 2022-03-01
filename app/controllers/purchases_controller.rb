d0d5d26c5b4c7a0e8ad4385c544ea26bd5f285bf
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-03-01 21:41:22 +0900

    クレジットカード機能の追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -24,7 +25,16 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:quantity_id, :size_id, :post_code, :area, :city, :street, :building_name, :phone_number).merge(
-      user_id: current_user.id, item_id: @item.id
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: purchase_params[:token],
+      currency: 'jpy'
     )
   end
 
