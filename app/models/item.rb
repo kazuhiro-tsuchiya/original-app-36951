dfb504bda131486cf3955dabd72da86f9a00b73d
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-03-03 21:40:25 +0900

    テストコードの記述(item, user, purchase))

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :item_name
-    validates :price
+    validates :price, numericality: { with: /\A[0-9]+\z/}
     validates :image
   end
 
