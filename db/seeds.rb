01e57da8d757cbba072ca75d59ca3b799a6ae6cf
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-03-09 16:00:26 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!(nickname: "管理者",
              email: "liverpool10@gmail.com",
-             password: "1a2b3c",
-             password_confirmation: "1a2b3c",
+             password: ENV["ADMIN_USER_PASSWORD"],
+             password_confirmation: ENV["ADMIN_USER_PASSWORD"],
              last_name: "原",
              first_name: "太郎",
              admin: true)
