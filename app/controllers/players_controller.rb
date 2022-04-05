77c70e4f365f40aca79a2a67cf917c35e2beb607
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-04-05 16:23:45 +0900

    選手登録ページの実装

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,4 +4,17 @@ class PlayersController < ApplicationController
     @players = Player.all
   end
 
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
 end
